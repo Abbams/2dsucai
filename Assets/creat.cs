bbe3c869e8bacbb626fd23dd586a5054ac4b97fd
abbams <wjs_gz@126.com>
2023-04-20 17:09:49 +0800

    123123

---
+++ Assets/creat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] enemies;
+    public float starttime = 6.0f;
+    public float timestep = 1.0f;
+    void EnemyFun()
+    {
+        int index = Random.Range(0, enemies.Length);
+        Instantiate(enemies[index], transform.position, transform.rotation);
+    }
+    void Start() 
+    {
+        InvokeRepeating("EnemyFun", starttime, timestep);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
